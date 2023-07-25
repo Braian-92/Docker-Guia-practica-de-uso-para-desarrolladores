4492e16a41d6ee2f1b2cb0e3f7f667af1eacdd77
Braian-92 <zamudiobraianhernan@gmail.com>
2023-07-25 02:54:59 -0300

    base proyecto node, instalado net-tools y webadmin

--- index01.php
+++ index01.php
@@ -182,3 +182,114 @@ PD: el directorio "/var/lib/mysql" lo sacamos de la documentación de dockerhub
 docker container rm -f eea (borramos el contenedor anterior forzosamente)
 
 al eliminar y recrear el volumen este sigue contienendo la información guardada en la base de datos
+
+
+##############  phpmyadmin  #################
+
+https://hub.docker.com/_/phpmyadmin
+
+docker container run \
+--name phpmyadmin \
+-d \
+-e PMA_ARBITRARY=1 \
+-p 8080:80 \
+phpmyadmin:5.2.1-apache
+
+## REDES ##
+
+DOCU networks = https://docs.docker.com/engine/tutorials/networkingcontainers/
+
+docker network ls (listamos las redes)
+
+docker network create world-app (creamos una nueva red)
+
+docker container ps -a (visualizamos los contenedores para sacar los IDs)
+############################### SALIDA #######################
+CONTAINER ID   IMAGE                     COMMAND                  CREATED          STATUS          PORTS                    NAMES
+81ce76fd2c20   phpmyadmin:5.2.1-apache   "/docker-entrypoint.…"   About a minute ago   Up About a minute   0.0.0.0:8080->80/tcp     phpmyadmin
+8f2f4c745d90   mariadb:lts-jammy         "docker-entrypoint.s…"   2 minutes ago        Up 2 minutes        0.0.0.0:3306->3306/tcp   world-db
+############################### SALIDA #######################
+
+docker network connect world-app 81c (unimos los contenedores a la red por el fragmento del ID)
+docker network connect world-app 8f2 (unimos los contenedores a la red por el fragmento del ID)
+
+docker network inspect world-app
+
+########### (en este fragmento podremos ver los 2 contenedores enlazados)
+Containers": {
+    "3514baff29cfb4de524d0eddc88a83afa3b13e58dddbf6ef04e381d202def6bc": {
+        "Name": "world-db",
+        "EndpointID": "04054040f7847a36a5fc2790710413fc2f5b9beeb8f0ce56107f99b03aee01a7",
+        "MacAddress": "02:42:ac:12:00:03",
+        "IPv4Address": "172.18.0.3/16",
+        "IPv6Address": ""
+    },
+    "d47f3ff9e8cc1bd7fcfa77066268663a854af540ab94ae99e2f089f4b769faad": {
+        "Name": "phpmyadmin",
+        "EndpointID": "3ca66ecb93af0cfab538f8423d86553e339c258a17c51f951bc91a9323dc4bd6",
+        "MacAddress": "02:42:ac:12:00:02",
+        "IPv4Address": "172.18.0.2/16",
+        "IPv6Address": ""
+    }
+},
+###########
+
+IMPORTANTE: el nombre del contenedor pasa a ser como un DNS "world-db" = 172.18.0.3, el cual podremos usar para conectarnos desde phpmyadmin
+
+ingresamos en phpmyadmin
+localhost:8080 (en mi caso se cacheo y tengo que abrirlo en incognito [no se sacar en linux el cache])
+
+world-db
+example-user
+user-password
+
+##### agregar la red a la instancia #####
+
+docker container run \
+-dp 3306:3306 \
+--name world-db \
+--env MARIADB_USER=example-user \
+--env MARIADB_PASSWORD=user-password \
+--env MARIADB_ROOT_PASSWORD=root-secret-password \
+--env MARIADB_DATABASE=world-db \
+--volume world-db:/var/lib/mysql \
+--network world-app \
+mariadb:lts-jammy
+
+docker container run \
+--network world-app \
+--name phpmyadmin \
+-d \
+-e PMA_ARBITRARY=1 \
+-p 8080:80 \
+phpmyadmin:5.2.1-apache
+
+-----------
+world-db
+example-user
+user-password
+-----------
+
+##### FIN agregar la red a la instancia #####
+
+subir la carpeta "archivos/cap03/nest-graphql" al servidor linux (nueva carpeta "docker")
+
+######### OPCIONAL INSTALAR WEBADMIN para gestionar el servidor desde el nevegador en windows de manera remota #########
+
+sudo apt update
+sudo apt install wget apt-transport-https
+wget http://www.webmin.com/download/deb/webmin-current.deb
+sudo dpkg -i webmin-current.deb
+sudo apt --fix-broken install
+https://TU_IP_UBUNTU:10000/
+
+#########################
+
+###### instalar net-tools ######
+sudo apt-get install net-tools
+
+ifconfig (192.168.1.44)
+
+192.168.1.44:10000 (entrar con usuario y contraseña del servidor linux)
+
+home/osboxes/docker
